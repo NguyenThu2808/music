a04ec728c31e305e58b5f3e77f4645173540d521
Phamduc8797 <phamduc8797@gmail.com>
2018-09-01 06:59:15 +0700

    admin-interface

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,8 @@
+//= require js/core/jquery.min.js
+//= require js/core/popper.min.js
+//= require js/core/bootstrap-material-design.min.js
+//= require js/plugins/perfect-scrollbar.jquery.min.js
+//= require js/plugins/chartist.min.js
+//= require js/plugins/bootstrap-notify.js
+//= require js/material-dashboard.js
+//= require demo/demo.js
