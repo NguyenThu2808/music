c0987ce37b0cb50e2eb2b71930e1170adf3d043d
Phamduc8797 <phamduc8797@gmail.com>
2018-09-10 11:07:21 +0700

    play-music

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,18 @@
 class SongsController < ApplicationController
+  before_action :load_song, only: :show
+
   def index
     @songs = Song.select_song.page(params[:page]).by_order.per Settings.per_page
+  end
+
+  def show; end
+
+  private
+
+  def load_song
+    @song = Song.find_by id: params[:id]
+    return if @song
+    flash[:danger] = t ".no_song"
+    redirect_to songs_url
   end
 end
